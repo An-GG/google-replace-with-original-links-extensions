c97a4f87ef0aff331bb8f2953b0fc1313a92f3d0
Ankush Girotra <ankush.girotra@gmail.com>
2021-07-13 12:16:16 -0500

    safari

---
+++ safari-google-replace-with-original-links/safari-google-replace-with-original-links Extension/SafariWebExtensionHandler.swift
@@ -0,0 +1,26 @@
+//
+//  SafariWebExtensionHandler.swift
+//  safari-google-replace-with-original-links Extension
+//
+//  Created by Ankush Girotra on 7/13/21.
+//
+
+import SafariServices
+import os.log
+
+let SFExtensionMessageKey = "message"
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+	func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+    
+}
